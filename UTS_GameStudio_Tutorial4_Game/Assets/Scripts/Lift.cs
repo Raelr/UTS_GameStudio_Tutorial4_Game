685c86eefb4cfcec68828cd0d952839eecaa8a0f
Alex Lin <peiming.lin@student.uts.edu.au>
2019-04-07 13:22:50 +1000

    Updated lift’s movement. Updated flashing effect when enemy is about to spawn from the door.

---
+++ UTS_GameStudio_Tutorial4_Game/Assets/Scripts/Lift.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lift : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move(Input.GetAxisRaw("Vertical"));
+    }
+
+    void Move(float movement){
+        transform.position += Vector3.up * movement * speed * Time.deltaTime;
+    }
+}
